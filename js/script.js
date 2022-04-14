19aa2b2ea41246319b97af7cf2a28ae961c3d417
Keva13 <volykevish@gmail.com>
2022-04-14 16:01:38 +0300

    made the menu work

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+$(document).ready(function(){
+    $(".scrollLink").click(function (event){
+		event.preventDefault();
+		var id  = $(this).attr('href');
+		var	top = $("."+id).offset().top;
+		$('body,html').animate({scrollTop: top}, 500);
+    })
+});
+
 $(".header__burger").click(()=>{
     $(".header__burger").toggleClass("active")
     $(".nav-header").toggleClass("active")
