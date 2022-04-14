c246fed6ba6899d5ce82145f654d1ab9e50fa318
Keva13 <volykevish@gmail.com>
2022-04-14 15:20:54 +0300

    add section 'contact us'

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 $(".header__burger").click(()=>{
     $(".header__burger").toggleClass("active")
     $(".nav-header").toggleClass("active")
+});
+$("#formSubmit").click((event)=>{
+    event.preventDefault()
 })
+
